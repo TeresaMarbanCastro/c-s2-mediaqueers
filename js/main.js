fac6baa5dbc7ba92c90cd8ade7c88909e3cfe4ac
Maria Virginia <mariav53@gmail.com>
2017-11-23 18:53:44 +0100

    js function to display hidden div

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
+//función para desplegar sección
 function editar(){
 document.getElementById('content').style.display = 'block';}
+
+//función para cerrar sección
+function cerrar(){
+document.getElementById('content').style.display = 'none';}
