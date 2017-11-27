011b783de0cc96c083cb0461c72cf6b97513d45e
Maria Virginia <mariav53@gmail.com>
2017-11-27 12:07:52 +0100

    add prompt function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 //función para desplegar sección
-
-
 function editar(idContent){
-
 document.getElementById(idContent).style.display = 'block';
+}
 
+//función para cerrar sección
+function cerrar(idContent){
+	document.getElementById(idContent).style.display = 'none';
 }
 
 
-//función para cerrar sección
-function ocultar(idContent){
-	document.getElementById(idContent).style.display = 'none';
+
+function rellenar(){
+	var datosNombre = prompt("Nombre:");
+	var datosApellido = prompt("Apellido:");
+
+	document.getElementById("datos-nombre").innerHTML= datosNombre;
+	document.querySelector("#datos-apellido").innerHTML = datosApellido;
+}
+
+function rellenarcontacto(){
+	var datosTelefono = prompt("Teléfono:");
+	var datosEmail = prompt("email:");
+
+	document.querySelector("#datos-telefono").innerHTML = datosTelefono;
+	document.querySelector("#datos-email").innerHTML = datosEmail;	
 }
